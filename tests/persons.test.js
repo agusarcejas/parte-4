94f49dada24798f0fb84a8dbeaed139327771005
agus75 <agus782009@gmail.com>
2023-04-10 01:46:40 -0300

    change initialPersons + add tests

--- tests/persons.test.js
+++ tests/persons.test.js
@@ -6,11 +6,11 @@ const { api, initialPersons, getAllNamePersons } = require('./helpers')
 beforeEach(async () => {
   await Person.deleteMany({})
 
-  const person1 = new Person(initialPersons[0])
-  await person1.save()
-
-  const person2 = new Person(initialPersons[1])
-  await person2.save()
+  // Forma secuencial de obtener todas las personas de initialPersons y que pueda añadir cuatas personas quiera
+  for (const person of initialPersons) {
+    const newPerson = new Person(person)
+    await newPerson.save()
+  }
 })
 
 test('persons are returned as json', async () => {
@@ -26,7 +26,7 @@ test('there are two notes', async () => {
 })
 
 test('the first note is about Arto Hellas', async () => {
-  const names = await getAllNamePersons()
+  const { names } = await getAllNamePersons()
   expect(names).toContain('Arto Hellas')
 })
 
@@ -42,8 +42,48 @@ test('a valid person can be added', async () => {
     .expect(201)
     .expect('Content-Type', /application\/json/)
 
-  const names = await getAllNamePersons()
+  const { names } = await getAllNamePersons()
   expect(names).toContain('Matti Luukkainen')
+})
+
+test('a person without name is not added', async () => {
+  const newPerson = {
+    number: '040-123456'
+  }
+
+  await api
+    .post('/api/persons')
+    .send(newPerson)
+    .expect(400)
+
+  const response = await api.get('/api/persons')
+  expect(response.body).toHaveLength(initialPersons.length)
+})
+
+test('a person can be deleted', async () => {
+  const { response: firstResponse } = await getAllNamePersons()
+  const { body: persons } = firstResponse
+  const personsToDelete = persons[0]
+
+  await api
+    .delete(`/api/persons/${personsToDelete.id}`)
+    .expect(204)
+
+  const { names, response: secondResponse } = await getAllNamePersons()
+
+  expect(secondResponse.body).toHaveLength(initialPersons.length - 1)
+
+  expect(names).not.toContain(personsToDelete.name)
+})
+
+test('a person that do not exist can not be deleted', async () => {
+  await api
+    .delete('/api/persons/1234')
+    .expect(400)
+
+  const { response } = await getAllNamePersons()
+
+  expect(response.body).toHaveLength(initialPersons.length)
 })
 
 afterAll(() => {
